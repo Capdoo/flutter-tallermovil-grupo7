21c9339e34f350732cd26022cb858e378a9698d7
Sebastian Vargas <vargaspizango1@gmail.com>
2022-06-04 10:24:04 -0500

    feat: add validation

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
+  static const numero = 10;
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
