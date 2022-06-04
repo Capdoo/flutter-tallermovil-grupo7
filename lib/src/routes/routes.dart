e597e3dd8691afcf37557063e7c37cb04fa2e9a9
Maurk <mauricio.lp@astrumla.com>
2022-06-04 13:42:39 -0500

    feat: home view finished

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/login.dart';
+import 'package:flutter_tallermovil_grupo7/src/screens/home.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/sign_up.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/sign_upp_app.dart';
 
-const initialRoute= "/landing";
+const initialRoute= "/home";
 
 final Map<String, WidgetBuilder> routes = {
   '/sign-up': (context) => const SignUp(),
   '/sign-up-app': (context) => const SignUpApp(),
   '/login': (context) => const Login(),
+  '/home': (context) => const Home()
 };
