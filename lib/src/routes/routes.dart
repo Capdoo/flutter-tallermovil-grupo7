219505965411cda2e19d49e02fb2d6de1d3240f5
ArianZambrano <arian.zambrano@unmsm.edu.pe>
2022-06-04 13:14:49 -0500

    feat: Adding landing page screen and route

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -1,11 +1,15 @@
+import 'dart:js';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/login.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/sign_up.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/sign_upp_app.dart';
+import 'package:flutter_tallermovil_grupo7/src/screens/landing_page.dart';
 
 const initialRoute= "/landing";
 
 final Map<String, WidgetBuilder> routes = {
+  '/landing': (context) => const Landing(),
   '/sign-up': (context) => const SignUp(),
   '/sign-up-app': (context) => const SignUpApp(),
   '/login': (context) => const Login(),
