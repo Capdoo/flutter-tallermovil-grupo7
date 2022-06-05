21f18c48f5fca0b702a7d0fd742a911a8124aa52
Sebastian Vargas <vargaspizango1@gmail.com>
2022-06-04 19:06:33 -0500

    feat: add my refuges section

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -3,16 +3,18 @@ import 'dart:js';
 import 'package:flutter/material.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/login.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/home.dart';
+import 'package:flutter_tallermovil_grupo7/src/screens/my_refugees.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/sign_up.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/sign_upp_app.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/landing_page.dart';
 
-const initialRoute= "/home";
+const initialRoute = "/home";
 
 final Map<String, WidgetBuilder> routes = {
   '/landing': (context) => const Landing(),
   '/sign-up': (context) => const SignUp(),
   '/sign-up-app': (context) => const SignUpApp(),
   '/login': (context) => const Login(),
+  '/my-refuges': (context) => const Refuges(),
   '/home': (context) => const Home()
 };
