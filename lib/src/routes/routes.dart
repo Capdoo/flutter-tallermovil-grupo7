494acd9f306e12701c00ed410020e092c42329f9
Sebastian Vargas <vargaspizango1@gmail.com>
2022-06-11 00:49:28 -0500

    feat: add my pets view

--- lib/src/routes/routes.dart
+++ lib/src/routes/routes.dart
@@ -3,6 +3,7 @@ import 'dart:js';
 import 'package:flutter/material.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/login.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/home.dart';
+import 'package:flutter_tallermovil_grupo7/src/screens/my_pets.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/my_refugees.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/sign_up.dart';
 import 'package:flutter_tallermovil_grupo7/src/screens/sign_upp_app.dart';
@@ -16,5 +17,6 @@ final Map<String, WidgetBuilder> routes = {
   '/sign-up-app': (context) => const SignUpApp(),
   '/login': (context) => const Login(),
   '/my-refuges': (context) => const Refuges(),
+  '/my-pets':(context) => const MyPets(),
   '/home': (context) => const Home()
 };
